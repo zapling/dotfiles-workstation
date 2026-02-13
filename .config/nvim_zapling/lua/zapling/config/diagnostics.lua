3d2fd90b61737faea7e46acd5892ece032efe9c4
Andreas Palm <andreaspalm98@gmail.com>
2026-02-13 10:18:49 +0100

    nvim: Fix broken diagnostic nerd font icons

--- .config/nvim_zapling/lua/zapling/config/diagnostics.lua
+++ .config/nvim_zapling/lua/zapling/config/diagnostics.lua
@@ -3,10 +3,10 @@ vim.diagnostic.config({
     underline = true,
     signs = {
         text = {
-            [vim.diagnostic.severity.ERROR] = '',
+            [vim.diagnostic.severity.ERROR] = '',
             [vim.diagnostic.severity.WARN] = '',
             [vim.diagnostic.severity.INFO] = '',
-            [vim.diagnostic.severity.HINT] = '',
+            [vim.diagnostic.severity.HINT] = '󰠠',
         },
     },
     float = {
